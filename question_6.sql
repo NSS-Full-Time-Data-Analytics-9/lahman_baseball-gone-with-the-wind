bc2bce1251c4a7bd576efbb0a14f80a4d6d597be
Alex Ennis <aedevelopes@gmail.com>
2023-02-16 10:28:37 -0600

    revised q6

--- question_6.sql
+++ question_6.sql
@@ -1,7 +1,9 @@
---Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base 
---attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
+--Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts 
+--which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.)
+--Consider only players who attempted at least 20 stolen bases.
 SELECT namefirst, namelast, yearid, ROUND((SB::numeric/(CS::numeric+SB::numeric)*100),2) as perc_stolen
 FROM people INNER JOIN batting USING(playerid)
 WHERE yearid = 2016
-AND (CS+SB) > 20
+AND (CS+SB) >= 20
 ORDER BY perc_stolen DESC
+LIMIT 1
